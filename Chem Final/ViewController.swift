c461d115948329423fbee8193bc2f2f1fab79cdf
Annie Wang <anniedwang02@gmail.com>
2018-05-18 09:30:06 -0400

    Initial Commit

---
+++ Chem Final/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chem Final
+//
+//  Created by Annie Wang on 5/18/18.
+//  Copyright © 2018 Annie Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
